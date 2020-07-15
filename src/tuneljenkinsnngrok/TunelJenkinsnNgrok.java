d8869232e93b0337eec3621a63379c4d6618e9bf
admin <admin@DESKTOP-F7PR10I>
2020-07-15 14:04:09 -0400

    firstCommit

---
+++ src/tuneljenkinsnngrok/TunelJenkinsnNgrok.java
@@ -0,0 +1,17 @@
+package tuneljenkinsnngrok;
+
+/**
+ *
+ * @author A.Belarbia BDEB
+ */
+public class TunelJenkinsnNgrok {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour "+args[1]+", " +args[0]);
+    }
+    
+}
